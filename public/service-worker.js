a0e41b6524876784e8455cec76db0fb8626e1a4f
christzelep <trendinggadgets23@gmail.com>
2025-06-24 14:44:24 +0300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,8 @@
-const CACHE_NAME = 'pawhealth-v3';
+const CACHE_NAME = 'pawhealth-v4';
 const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/service-worker.js',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
   'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap'
 ];
@@ -11,13 +10,36 @@ const urlsToCache = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        
+        return fetch(event.request).then(response => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
+      })
+  );
 });
 
 self.addEventListener('activate', event => {
