53035afc3395a0242991092152ff585a1efec8f5
christzelep <trendinggadgets23@gmail.com>
2025-06-24 14:18:09 +0300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,11 @@
-const CACHE_NAME = 'pawhealth-v1';
+const CACHE_NAME = 'pawhealth-v3';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/manifest.json',
+  '/service-worker.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap'
 ];
 
 self.addEventListener('install', event => {
@@ -16,4 +18,19 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
